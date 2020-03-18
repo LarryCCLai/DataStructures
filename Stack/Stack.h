1e5c96c3b7fdf03a5b547cff60cde2846d43ef62
ChunChihLai <t106360217@ntut.org.tw>
2020-03-18 18:21:19 +0800

    Stack_Stack.h

--- Stack/Stack.h
+++ Stack/Stack.h
@@ -1,1 +1,20 @@
+#ifndef STACK_H
+#define STACK_H
+template<class DataType>
+class Stack {
+private:
+	int topIdx;
+	int size;
+	DataType* Datas;
+	
+public:
+	Stack();
+	~Stack();
+	void push(DataType data);
+	void pop();
+	DataType top();
+	int size();
+	bool empty();
+};
 
+#endif // !STACK_H
