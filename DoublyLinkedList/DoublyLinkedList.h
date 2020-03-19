87842472d36ea43b4a87540e74a17399b38971ac
ChunChihLai <t106360217@ntut.org.tw>
2020-03-20 07:46:23 +0800

    DoublyLinkedList, h file

--- DoublyLinkedList/DoublyLinkedList.h
+++ DoublyLinkedList/DoublyLinkedList.h
@@ -1,12 +1,20 @@
 #ifndef DoublyLinkedList_H
 #define DoublyLinkedList_H
-
+#include"DoublyLinkedListNode.h"
 class DoublyLinkedList{
 private:
-
+	DoublyLinkedListNode* head;
+	DoublyLinkedListNode* tail;
+	int size;
 public:
 	DoublyLinkedList();
 	~DoublyLinkedList();
+	void Insert(int idx, int data);
+	void Delete(int idx);
+	void PushFront(int data);
+	void PushBack(int data);
+	int Search(int data);
+	void Reverse();
 };
 
 #endif // !DoublyLinkedList_H
